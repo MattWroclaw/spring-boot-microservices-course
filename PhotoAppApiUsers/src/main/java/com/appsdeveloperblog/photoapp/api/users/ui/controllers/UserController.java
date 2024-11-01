d5276f332a032e89302e757af326538ad09ad74c
Mateusz B <mateusz.bialos@atos.net>
2024-11-01 23:08:58 +0100

    INtroduced H2 database

--- PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UserController.java
+++ PhotoAppApiUsers/src/main/java/com/appsdeveloperblog/photoapp/api/users/ui/controllers/UserController.java
@@ -1,10 +1,10 @@
 package com.appsdeveloperblog.photoapp.api.users.ui.controllers;
 
+import com.appsdeveloperblog.photoapp.api.users.ui.model.CreateUserRequestModel;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/users")
@@ -14,7 +14,12 @@ public class UserController {
     private Environment env;
 
     @GetMapping("/status/check")
-    public String status(){
-        return "Working on port = "  + env.getProperty("local.server.port");
+    public String status() {
+        return "Working on port = " + env.getProperty("local.server.port");
+    }
+
+    @PostMapping
+    public String createUser(@Valid  @RequestBody  CreateUserRequestModel userDetails) {
+        return "Create user was called";
     }
 }
